1ccc92334078d0b20ae9aadc1eb3e122db02dc1c
hansen <hansen@elephantroom.com.au>
2021-03-03 10:47:56 +1100

    form test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { navigate } from 'gatsby-link'
-import Layout from '../layout'
+import React from 'react';
+import { navigate } from 'gatsby-link';
+import Layout from '../layout';
 
 function encode(data) {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
+    .join('&');
 }
 
 export default function Contact() {
-  const [state, setState] = React.useState({})
+  const [state, setState] = React.useState({});
 
   const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
+    e.preventDefault();
+    const form = e.target;
+    fetch('https://condescending-varahamihira-b8a5c2.netlify.app/contact/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
@@ -27,52 +27,45 @@ export default function Contact() {
       }),
     })
       .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
+      .catch((error) => alert(error));
+  };
 
   return (
     <Layout>
       <h1>Contact</h1>
-      <form
-        name="contact"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
+      <form name='contact' method='post' action='/thanks/' data-netlify='true' data-netlify-honeypot='bot-field' onSubmit={handleSubmit}>
         {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
+        <input type='hidden' name='form-name' value='contact' />
         <p hidden>
           <label>
-            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+            Don’t fill this out: <input name='bot-field' onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
             Your name:
             <br />
-            <input type="text" name="name" onChange={handleChange} />
+            <input type='text' name='name' onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
             Your email:
             <br />
-            <input type="email" name="email" onChange={handleChange} />
+            <input type='email' name='email' onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
             Message:
             <br />
-            <textarea name="message" onChange={handleChange} />
+            <textarea name='message' onChange={handleChange} />
           </label>
         </p>
         <p>
-          <button type="submit">Send</button>
+          <button type='submit'>Send</button>
         </p>
       </form>
     </Layout>
-  )
+  );
 }
